010186e7118330ff8fd3f693d179bc920e5fa295
Daniel Vasquez <danvasq@gmail.com>
2019-06-07 00:48:22 -0700

    Factored out the Navbar to custom MenuBar component. Changed background sizing. Added useWindowWidth hook.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,34 +3,22 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import { 
   About,
   Contact,
   InfoFooter } from '../About';
 import LandingPage, { Home } from '../Landing';
 import Works from '../Works';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { LinkContainer } from 'react-router-bootstrap'
 import * as ROUTES from '../../constants/routes';
+import MenuBar from './MenuBar';
 
 function App(props) {
   return (
-    <div className="bg-cloud vh-100">
+    <div className="">
       <Router>
         <div>
           <header>
-          <Navbar expand="sm">
-            <LinkContainer to={ROUTES.LANDING}><Navbar.Brand className="mb-0 h1">Daniel Vasquez</Navbar.Brand></LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-                <LinkContainer to={ROUTES.WORKS}><Nav.Link>Work</Nav.Link></LinkContainer>
-                <LinkContainer to={ROUTES.ABOUT}><Nav.Link>About</Nav.Link></LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+            <MenuBar />
           </header>
           <main role="main">
             <Route exact path={ROUTES.LANDING} component={LandingPage} />
