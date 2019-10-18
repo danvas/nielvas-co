17a5318ea9ffb88656d8c600ef9d8739da338b4c
Daniel Vasquez <daniel@fnx.tech>
2019-10-17 22:24:32 -0700

    Updated About statement.

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -6,7 +6,7 @@ function About(props) {
   return (
     <div className="p-5">
       <br></br>
-      <h1>I design & develop software for automating workflows in 3D content creation and integrating them with cloud services.</h1>
+      <h1>I design & develop cloud enabled software for automating workflows in 3D content creation.</h1>
       <br></br>
       <p>In the past 5 years, I created software tools for the visual effects pipeline in feature films, such as <a className="text-primary" target="_blank" href="https://www.imdb.com/name/nm9678054/">Black Panther</a> and Aquaman.</p>
       <p>As a web developer I created <a target="_blank" href="https://lyddy.stream/nielvas">Lyddy</a> — a sound-sharing social networking web application which allows users to post third-party audio sources to their stream, follow other user streams, and organize posts by hashtags.</p>
