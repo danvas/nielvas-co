c77aa7e275e4d5f70787d299147584a58002db22
Daniel Vasquez <danvasq@gmail.com>
2019-10-23 00:22:33 -0700

    html header tweaks.

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -6,9 +6,9 @@ function About(props) {
   return (
     <div className="p-5">
       <br></br>
-      <h1>I design & develop cloud enabled software for automating workflows in 3D content creation.</h1>
+      <h1>I develop cloud enabled software for automating workflows in 3D content creation.</h1>
       <br></br>
-      <p>In the past 5 years, I created software tools for the visual effects pipeline in feature films, such as <a className="text-primary" target="_blank" href="https://www.imdb.com/name/nm9678054/">Black Panther</a> and Aquaman.</p>
+      <p>In the past 5 years, I created software tools for the visual effects pipeline in feature films such as <a className="text-primary" target="_blank" href="https://www.imdb.com/name/nm9678054/">Black Panther</a> and Aquaman.</p>
       <p>As a web developer I created <a target="_blank" href="https://lyddy.stream/nielvas">Lyddy</a> — a sound-sharing social networking web application which allows users to post third-party audio sources to their stream, follow other user streams, and organize posts by hashtags.</p>
       <p>Currently I'm a Software Engineer at <a target="_blank" href="https://www.fnx.tech">FNX Technologies</a>, helping to integrate 3D visualization for the product lifecycle in the Fashion and Apparel supply chain.</p>
       <div className="text-center my-5">
