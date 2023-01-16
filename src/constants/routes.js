226db12ece15f155b64a04a219347423e150b475
Daniel Vasquez <nielvas.dev@gmail.com>
2023-01-16 14:22:26 -0800

    chore!: removed old src code

--- src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const LANDING = '/';
-export const HOME = '/home';
-export const ABOUT = '/about';
-export const WORKS = '/work';
-export const IDEAS = '/ideas';
-export const CONTACT = '/contact';
