cc2164995cacd9f112b196d9462370984743f378
Daniel Vasquez <nielvas.dev@gmail.com>
2023-03-22 16:27:05 -0600

    style: social media icons and color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,41 @@
 
+const LinkedInIcon = () => {
+  return (
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-linkedin mb-1" viewBox="0 0 16 16">
+      <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+    </svg>
+  )
+}
+
+const GitHubIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github mb-1" viewBox="0 0 16 16">
+      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+    </svg>
+
+  )
+}
+
+
+const IMDbIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="mb-1" width="18" height="18" fill="currentColor" viewBox="0 0 448 512">
+      <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM21.3 229.2H21c.1-.1.2-.3.3-.4zM97 319.8H64V192h33zm113.2 0h-28.7v-86.4l-11.6 86.4h-20.6l-12.2-84.5v84.5h-29V192h42.8c3.3 19.8 6 39.9 8.7 59.9l7.6-59.9h43zm11.4 0V192h24.6c17.6 0 44.7-1.6 49 20.9 1.7 7.6 1.4 16.3 1.4 24.4 0 88.5 11.1 82.6-75 82.5zm160.9-29.2c0 15.7-2.4 30.9-22.2 30.9-9 0-15.2-3-20.9-9.8l-1.9 8.1h-29.8V192h31.7v41.7c6-6.5 12-9.2 20.9-9.2 21.4 0 22.2 12.8 22.2 30.1zM265 229.9c0-9.7 1.6-16-10.3-16v83.7c12.2.3 10.3-8.7 10.3-18.4zm85.5 26.1c0-5.4 1.1-12.7-6.2-12.7-6 0-4.9 8.9-4.9 12.7 0 .6-1.1 39.6 1.1 44.7.8 1.6 2.2 2.4 3.8 2.4 7.8 0 6.2-9 6.2-14.4z" />
+    </svg>
+  )
+}
+
 function Footer() {
   return (
     <div className="d-print-none p-4">
       <div className="d-flex align-items-center">
-        <a className="fs-6 nv-outlined-link fw-light me-4" target="_blank" href="https://www.linkedin.com/in/danvas">LinkedIn</a>
-        <a className="fs-6 nv-outlined-link fw-light me-4" target="_blank" href="https://github.com/danvas">Github</a>
-        <a className="fs-6 nv-outlined-link fw-light me-4" target="_blank" href="https://www.imdb.com/name/nm9678054">IMDb</a>
-        <div className="ms-auto text-end">
-          <a className="nv-outlined-link fw-light" target="_blank" href="mailto:d@nielvas.co">d@nielvas.co</a>
 
+        <a className="nv-outlined-link fw-light me-4" target="_blank" href="https://www.linkedin.com/in/danvas"><LinkedInIcon /><span> LinkedIn</span></a>
+        <a className="nv-outlined-link fw-light me-4" target="_blank" href="https://github.com/danvas"><GitHubIcon /><span> Github</span></a>
+        <a className="nv-outlined-link fw-light" target="_blank" href="https://www.imdb.com/name/nm9678054"><IMDbIcon /><span> IMDb</span></a>
+        <div className="ms-auto text-end">
+          <a className="nv-outlined-link fw-light w-100" target="_blank" href="mailto:d@nielvas.co">d@nielvas.co</a>
           <div className="small text-muted pe-1">Vancouver, Canada</div>
         </div>
       </div>
