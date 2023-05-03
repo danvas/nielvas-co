7052bee8b69c47e85dfe21384157d4a491b6b84d
Daniel Vasquez <nielvas.dev@gmail.com>
2023-05-02 23:43:48 -0700

    fix: move head html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
-import Navbar from "../components/NavBar";
-import Footer from "../components/Footer";
+import Head from 'next/head'
+import Navbar from "../components/NavBar"
+import Footer from "../components/Footer"
 import '../styles.scss'
 
 function Root({ Component, pageProps }) {
@@ -8,6 +9,11 @@ function Root({ Component, pageProps }) {
   const enabledBg = router.asPath === "/" ? "nv-cloud-bg" : "";
   return (
     <div id="root-container" className={`d-flex flex-column ${enabledBg}`}>
+      <Head>
+        <title>Daniel Vasquez // 3D Software Developer</title>
+        <link rel="icon" href="https://symbl-world.akamaized.net/i/webp/78/4c77a8fb21eeb70940ce079977345c.webp" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
-import Head from 'next/head'
 import Link from 'next/link'
 
 function LandingPage() {
   return (
     <div className="h-100">
-      <Head>
-        <title>Daniel Vasquez // 3D Software Developer</title>
-        <link rel="icon" href="https://symbl-world.akamaized.net/i/webp/78/4c77a8fb21eeb70940ce079977345c.webp" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      </Head>
-
       <div className="container h-100 d-flex flex-column align-items-center justify-content-center w-75">
         <p className="fs-1 fw-light text-dark p-4">
           Hey, I'm Daniel <br /> - a Software Developer helping <span className="fw-light text-light">automate 3D workflows</span> for Creatives, Artists, and Businesses
