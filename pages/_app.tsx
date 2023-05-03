7bc6d99348d22f06bcc7887e7d887d32626aa6ed
Daniel Vasquez <nielvas.dev@gmail.com>
2023-05-03 13:52:41 -0700

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import Script from 'next/script'
 import Navbar from "../components/NavBar"
 import Footer from "../components/Footer"
 import '../styles.scss'
@@ -13,6 +14,14 @@ function Root({ Component, pageProps }) {
         <title>Daniel Vasquez // 3D Software Developer</title>
         <link rel="icon" href="https://symbl-world.akamaized.net/i/webp/78/4c77a8fb21eeb70940ce079977345c.webp" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DFH13LLV4W"></script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DFH13LLV4W');`}
+        </Script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
