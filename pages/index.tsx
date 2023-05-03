be265068721854626012e7dc0f610a6e8b0bbf1f
Daniel Vasquez <nielvas.dev@gmail.com>
2023-05-02 23:38:53 -0700

    fix: copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ function LandingPage() {
 
       <div className="container h-100 d-flex flex-column align-items-center justify-content-center w-75">
         <p className="fs-1 fw-light text-dark p-4">
-          Hey, I'm Daniel <br /> - a Software Developer helping <span className="fw-light text-light">automate 3D workflows</span> for Creatives, Artists, and Businesses.
+          Hey, I'm Daniel <br /> - a Software Developer helping <span className="fw-light text-light">automate 3D workflows</span> for Creatives, Artists, and Businesses
         </p>
         <div className="p-5" >
           <Link
