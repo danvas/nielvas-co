1e73afc744b497168c1f63dd6c8d3f6718c1d924
Daniel Vasquez <nielvas.dev@gmail.com>
2023-06-01 17:51:34 -0400

    fix: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,18 +10,24 @@ function Root({ Component, pageProps }) {
   const enabledBg = router.asPath === "/" ? "nv-cloud-bg" : "";
   return (
     <div id="root-container" className={`d-flex flex-column ${enabledBg}`}>
+      <Script
+        strategy="beforeInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-DFH13LLV4W">
+      </Script>
+      <Script
+        strategy="afterInteractive"
+      >
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-DFH13LLV4W');`}
+      </Script>
       <Head>
         <title>Daniel Vasquez // 3D Software Developer</title>
         <link rel="icon" href="https://symbl-world.akamaized.net/i/webp/78/4c77a8fb21eeb70940ce079977345c.webp" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DFH13LLV4W"></script>
-        <Script>
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-DFH13LLV4W');`}
-        </Script>
       </Head>
       <Navbar />
       <Component {...pageProps} />
